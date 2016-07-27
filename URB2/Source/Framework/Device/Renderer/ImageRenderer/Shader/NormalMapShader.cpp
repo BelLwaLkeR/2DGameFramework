0436b89d69c17050d93b1779254262112d5ea9e4
BelLwaLkeR <secreter_d@yahoo.co.jp>
2016-07-27 17:42:07 +0900

    NormalMapShader.cpp整形 不要ファイル削除

--- URB2/Source/Framework/Device/Renderer/ImageRenderer/Shader/NormalMapShader.cpp
+++ URB2/Source/Framework/Device/Renderer/ImageRenderer/Shader/NormalMapShader.cpp
@@ -4,6 +4,7 @@
 #include <Source/Framework/Device/Renderer/ImageRenderer/Shader/ShaderManager.h>
 #include <Source/Framework/Device/Renderer/ImageRenderer/Shader/DxLibShader.h>
 #include <Source/Framework/Device/Renderer/ImageRenderer/LightingAggregate.h>
+#include <Source/Utility/Type/Color.h>
 #include <Source/Utility/DxLibUtility/DxLibPixelShaderLoader.h>
 
 framework::NormalMapShader::NormalMapShader(){
@@ -15,86 +16,31 @@ framework::NormalMapShader::~NormalMapShader(){
 }
 
 void framework::NormalMapShader::attachShader(util::SharedPtr<util::ImageData>* targetImage){
-////	DxLibShaderFunction::setUsePixelShader(m_Shader);
-////	DxLibShaderFunction::setTexture(0, (*targetImage)->getTexture());
-////	DxLibShaderFunction::setTexture(1, (*targetImage)->getNormalMap());
-////	DxLibShaderFunction::setTexture(2, (*targetImage)->getReflectionMap());
-////
-////
-//////	std::array<float, 3> relativeLightPosition = { 100 - m_pImageData->getPosition().X, 100 - m_pImageData->getPosition().Y, 0 };
-////	std::vector<float>		relativeLightPosition	= { 100 - (*targetImage)->getPosition().X, 100 - (*targetImage)->getPosition().Y, 0 };
-////	std::array<float, 3>	ambientLightColor		= { 1.0f, 0.2f, 0.2f };
-////	std::array<float, 3>	LightColor				= { 1.f, 1.f, 1.f };
-////
-////
-////
-////	DxLibShaderFunction::setValue(0, (*targetImage)->getSize());
-////	DxLibShaderFunction::setAmbientLight(2);
-//	DxLibShaderFunction::setLightColor(5, (*targetImage)->getPosition());
-////	DxLibShaderFunction::setValueArray(8, relativeLightPosition);
-////	DxLibShaderFunction::setValue(11, 5);
-////	SetPSConstSFArray(2, &ambientLightColor[0], 3);
-////
-////
-////	DrawPolygon2DToShader((*targetImage)->getVertexPointer(), 2);
-////	DeleteShader(m_Shader);
-//
-//	int imaHdl = (*targetImage)->getTexture();
-//	int nmlHdl = (*targetImage)->getNormalMap();
-//	int refHdl = (*targetImage)->getReflectionMap();
-//
-//	ClearDrawScreen();
-//	//	DrawGraph(m_posX - m_sizeX/2, m_posY - m_sizeY/2,m_ImageHandle, FALSE);
-//	SetUsePixelShader(m_Shader);
-//	SetUseTextureToShader(0, imaHdl);
-//	SetUseTextureToShader(1, nmlHdl);
-//	SetUseTextureToShader(2, refHdl);
-//
-//
-//	std::array<float, 3> relativeLightPosition	= { 10	, 10,0 };
-//	std::array<float, 3> ambientLightColor		= { 0.3f , 0.3f , 0.3f };
-//	std::array<float, 3> LightColor				= { 1.f	, 1.f , 1.f };
-//
-//
-//	SetPSConstSF(0, 32);
-//	SetPSConstSF(1, 32);
-//	SetPSConstSFArray(2, &ambientLightColor[0], 3);
-//	SetPSConstSFArray(5, &LightColor[0], 3);
-//	SetPSConstSFArray(8, &relativeLightPosition[0], 3);
-//	SetPSConstSF(11, 5.f);
-//
-//	//	printfDx("\t\t\t\t\t%f, %f, %f\n", m_pLight.lock()->getPosition().x, m_pLight.lock()->getPosition().y, m_pLight.lock()->getPosition().z);
-//
-//	DrawPolygon2DToShader((*targetImage)->getVertexPointer(), 2);
-//	DeleteShader(m_Shader);
-//
-//
+	util::WeakPtr<util::ImageData> pTargetImage = *targetImage;
+	DxLibShaderFunction::setUsePixelShader(m_ShaderHandle);
+	DxLibShaderFunction::setTexture(0, pTargetImage->getTexture());
+	DxLibShaderFunction::setTexture(1, pTargetImage->getNormalMap());
+	DxLibShaderFunction::setTexture(2, pTargetImage->getReflectionMap());
 
-	SetUsePixelShader(m_ShaderHandle);
-	SetUseTextureToShader(0, (*targetImage)->getTexture());
-	SetUseTextureToShader(1, (*targetImage)->getNormalMap());
-	SetUseTextureToShader(2, (*targetImage)->getReflectionMap());
-	util::WeakPtr<framework::Light> light= SGLT_LIGHT_AGGREGATE->getAffectLight((*targetImage)->getPosition()).front();
-	util::Vector2 lPos = *light->pPosition;
+	const Light&			light						= *SGLT_LIGHT_AGGREGATE->getAffectLight((*targetImage)->getPosition()).front();
+	const util::Color&		lightColor					= light.color;
+	const util::Vector2&	lightPosition				= *(light.pPosition);
+	const util::Vector2&	targetPosition				= pTargetImage->getPosition();
+	float					lightPositionZ				= 10.f;
+	std::vector<float>		relativeLightPosition		= { lightPosition.X - targetPosition.X, lightPosition.Y - targetPosition.Y, lightPositionZ };
 
-//	std::array<float, 3>	relativeLightPosition	= { 10.f, 10.f, 20.f };
-	std::vector<float>		relativeLightPosition	= { lPos.X - (*targetImage)->getPosition().X, lPos.Y - (*targetImage)->getPosition().Y, 10 };
-	std::array<float, 3>	ambientLightColor		= { 0.3f, 0.3f, 0.3f };
-	std::array<float, 3>	LightColor				= { 1.f, 1.f, 1.f };
+	const util::Color&		ambientLight				= *SGLT_LIGHT_AGGREGATE->getAmbientLight();
+	std::vector<float>		toShaderAmbientLightColor	= { ambientLight.getRedF()	, ambientLight.getGreenF()	, ambientLight.getBlueF() };
+	std::vector<float>		toShaderLightColor			= { lightColor.getRedF()	, lightColor.getGreenF()	, lightColor.getBlueF() };
 
+	DxLibShaderFunction::setValue		(0	, pTargetImage->getSize().X	);
+	DxLibShaderFunction::setValue		(1	, pTargetImage->getSize().Y	);
+	DxLibShaderFunction::setValueArray	(2	, toShaderAmbientLightColor	);
+	DxLibShaderFunction::setValueArray	(5	, toShaderLightColor		);
+	DxLibShaderFunction::setValueArray	(8	, relativeLightPosition		);
+	DxLibShaderFunction::setValue		(11	, light.intencity);
 
-	SetPSConstSF(0, 32);
-	SetPSConstSF(1, 32);
-	SetPSConstSFArray(2, &ambientLightColor[0], 3);
-	SetPSConstSFArray(5, &LightColor[0], 3);
-	SetPSConstSFArray(8, &relativeLightPosition[0], 3);
-	SetPSConstSF(11, 5.f);
-
-	//	printfDx("\t\t\t\t\t%f, %f, %f\n", m_pLight.lock()->getPosition().x, m_pLight.lock()->getPosition().y, m_pLight.lock()->getPosition().z);
-
-	DrawPolygon2DToShader((*targetImage)->getVertexPointer(), 2);
-//	DeleteShader(m_ShaderHandle);
-
+	DxLibImageRenderer::drawImage(*pTargetImage);
 }
 
 void framework::NormalMapShader::deleteShader(){
